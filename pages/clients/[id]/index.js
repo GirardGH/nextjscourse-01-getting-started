118dba5ea2c0265b6be060618c55e04362e91f5f
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-04-26 22:53:03 +0200

    second commit

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function ClientProjectPage() {
-    const router = useRouter();
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
 
-    console.log(router.pathname);
-    console.log(router.query);
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
 
   return (
     <div>
       <h1>ClientProjectPage</h1>
+      <ul>
+        {clients.map((client) => (client => <li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+        ))}
+      </ul>
     </div>
   );
 }
